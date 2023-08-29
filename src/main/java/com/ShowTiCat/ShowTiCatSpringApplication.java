b8d9ffbc69d4006734907c7c28bbb8b52fd2af37
HongJayoung <ghddlwkdud@naver.com>
2023-08-29 18:08:56 +0900

    first commit

---
+++ src/main/java/com/ShowTiCat/ShowTiCatSpringApplication.java
@@ -0,0 +1,13 @@
+package com.ShowTiCat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowTiCatSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShowTiCatSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ShowTiCat/ShowTiCatSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ShowTiCat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShowTiCatSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
