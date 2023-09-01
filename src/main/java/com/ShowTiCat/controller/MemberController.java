2b55a0ee321d421dac5a1bae00d1edbaa62113f3
HongJayoung <ghddlwkdud@naver.com>
2023-09-02 00:15:31 +0900

    create myPage

--- src/main/java/com/ShowTiCat/controller/MemberController.java
+++ src/main/java/com/ShowTiCat/controller/MemberController.java
@@ -4,21 +4,53 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.ShowTiCat.repository.MemberRepository;
+import com.ShowTiCat.repository.ReservationRepository;
+import com.ShowTiCat.repository.ReviewRepository;
 import com.ShowTiCat.vo.MemberVO;
 
 @Controller
-@RequestMapping("/ShowTiCat")
 public class MemberController {
 
 	@Autowired
 	MemberRepository mRepo;
 	
+	@Autowired
+	ReservationRepository reRepo;
+	
+	@Autowired
+	ReviewRepository rRepo;
+	
 	@GetMapping("/myPage")
-	public String myPage(HttpSession session) {
+	public String myPage() {
 		return "/myPage/myPage";
+	}
+	
+	@GetMapping("/myPage/myReservation")
+	public String myReservation(HttpSession session, Model model) {
+		MemberVO m = (MemberVO) session.getAttribute("member");
+		model.addAttribute("reservList", reRepo.findByMemberId(m.getMemberId()));
+		return "/myPage/myReservation";
+	}
+	
+	@GetMapping("/myPage/myReview")
+	public String myReview(HttpSession session, Model model) {
+		MemberVO m = (MemberVO) session.getAttribute("member");
+		model.addAttribute("reviewList", rRepo.findByMemberId(m.getMemberId()));
+		return "/myPage/myReview";
+	}
+	
+	@GetMapping("/myPage/updateInfo")
+	public String updateInfo() {
+		return "/myPage/updateInfo";
+	}
+	
+	@GetMapping("/myPage/deleteAccount")
+	public String deleteAccount() {
+		return "/myPage/deleteAccount";
 	}
 }
--- src/main/java/com/ShowTiCat/repository/ReservationRepository.java
+++ src/main/java/com/ShowTiCat/repository/ReservationRepository.java
@@ -1,9 +1,15 @@
 package com.ShowTiCat.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.ShowTiCat.vo.ReservationVO;
+import com.ShowTiCat.vo.ReviewVO;
 
 public interface ReservationRepository extends CrudRepository<ReservationVO, Long>{
 
+	@Query(value = "select * from reservation where member_member_id = ?1", nativeQuery = true)
+	List<ReservationVO> findByMemberId(String memberId);
 }
